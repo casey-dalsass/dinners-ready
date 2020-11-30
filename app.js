b02544f20021d102e5c19d725b2f640375f5d5cf
Glitch (cherry-harmonious-alyssum) <none>
2020-11-30 19:03:55 +0000

    🖼🚣 Checkpoint ./public/assets/css/main.css:25675174/6887 ./public/index.html:25675174/48616 ./app.js:25675174/2751

--- app.js
+++ app.js
@@ -13,23 +13,23 @@ app.use(express.static("public"));
 // Parse the body of requests automatically
 app.use(bodyParser.json());
 
-app.get("/api/compliments", async (req, res) => {
+app.get("/api/messages", async (req, res) => {
   // Get a list of messages sent from a specific number
-  const sentMessages = await client.messages.list({from: twilioPhoneNumber});
+  const sentMessages = await client.messages.list({ from: twilioPhoneNumber });
   // Gather only the body of those messages for sending to the client
-  const compliments = sentMessages.map(message => message.body);
-  res.json(compliments);
+  const dinnerMessage = sentMessages.map((message) => message.body);
+  res.json(dinnerMessage);
 });
 
-app.post("/api/compliments", async (req, res) => {
+app.post("/api/messages", async (req, res) => {
   const to = req.body.to;
   const from = process.env.TWILIO_PHONE_NUMBER;
-  const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers.referer}`;
+  const body = `${req.body.sender} says, ${req.body.receiver} ${req.body.message}. Thank you for using ${req.headers.referer}`;
   // Send a message
-  try{
-      await client.messages.create({to, from, body});
-  } catch(err) {
-    res.status(err.status).json({ success: false, message: err.message })
+  try {
+    await client.messages.create({ to, from, body });
+  } catch (err) {
+    res.status(err.status).json({ success: false, message: err.message });
   }
 
   res.json({ success: true });
