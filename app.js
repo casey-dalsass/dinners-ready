1169b45f2cca32d071c3fede2314bcdde9a93930
Glitch (cherry-harmonious-alyssum) <none>
2020-11-30 02:40:07 +0000

    👽☔️ Checkpoint ./app.js:25675174/251

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.get("/api/compliments", async (req, res) => {
-  // TODO: Get a list of messages sent from a specific number
+  // Get a list of messages sent from a specific number
   const sentMessages = await client.messages.list({from: twilioPhoneNumber});
-  // TODO: Gather only the body of those messages for sending to the client
+  // Gather only the body of those messages for sending to the client
   const compliments = sentMessages.map(message => message.body);
   res.json(compliments);
 });
@@ -25,8 +25,13 @@ app.post("/api/compliments", async (req, res) => {
   const to = req.body.to;
   const from = process.env.TWILIO_PHONE_NUMBER;
   const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers.referer}`;
-  // TODO: Send a message
-  await client.messages.create({to, from, body});
+  // Send a message
+  try{
+      await client.messages.create({to, from, body});
+  } catch(err) {
+    res.status(err.status).json({ success: false, message: err.message })
+  }
+
   res.json({ success: true });
 });
 
